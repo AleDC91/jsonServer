8cc6a5e5a84721d264baa40830fd23f4fec88f53
ale <ale91.dallas@gmail.com>
2023-12-21 14:34:54 +0100

    logout added

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 let logged = false;
 let username = "";
+let isAdmin = false;
 const token = localStorage.getItem("token");
-
 if (token) {
   logged = true;
+  isAdminfromToken(token);
   if (document.referrer.includes("login.html")) {
     let searchParams = new URLSearchParams(window.location.search);
     username = searchParams.get("name");
@@ -27,6 +28,7 @@ async function getUsernameFromToken(token) {
   jwt_elements = token.split(".");
   token_body = atob(jwt_elements[1]);
   json_token_body = JSON.parse(token_body);
+  console.log(json_token_body)
   userId = json_token_body.sub;
   fetch(URL + userId)
     .then((response) => response.json())
@@ -41,9 +43,34 @@ async function getUsernameFromToken(token) {
 if (logged) {
   document.querySelector(".login-btn").style.display = "none";
   document.querySelector(".register-btn").style.display = "none";
+  document.querySelector(".logout-btn").style.display = "block";
+  document.querySelector(".logout-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    location.href = "index.html"
+
+  })
 }
 
 function updateUsername(username) {
   console.log(username);
   document.querySelector(".ciao span").innerHTML = username;
+}
+
+function isAdminfromToken(token){
+  const URL = "http://localhost:3000/users/";
+  jwt_elements = token.split(".");
+  token_body = atob(jwt_elements[1]);
+  json_token_body = JSON.parse(token_body);
+  // console.log(json_token_body)
+  userId = json_token_body.sub;
+  fetch(URL + userId)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data.isAdmin)
+      if(data.isAdmin === true){
+        isAdmin = true;
+        return isAdmin;
+      }
+    });
 }
